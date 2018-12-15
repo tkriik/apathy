7e8149b66e8998fee5a06a690cb07a8248b298ef
Tanel Kriik <tanel.kriik+github@gmail.com>
2018-12-15 13:48:07 +0200

    Add profiling utility

---
+++ profile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Google perftools required
+
+set -e
+set -x
+
+make clean profile
+
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so CPUPROFILE=cpu_profile ./apathy $1
+
+google-pprof --gv ./apathy ./cpu_profile
